b7690afbb18184c782669dd07b811e4495d608e4
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2025-04-27 17:44:04 -0400

    add project files

---
+++ README.md
@@ -0,0 +1,41 @@
+# Rule 110
+
+An implementation [Rule 110](https://en.wikipedia.org/wiki/Rule_110) of written in standard C (C99) and uses GNU Make for building.
+
+![Rule 110 Demo](./docs/rule110-demo.png)
+
+## Usage
+
+To use the library in your project, include the header file.
+
+```c
+#include "rule110.h"
+```
+
+### Installation
+
+Clone the repository.
+
+```bash
+git clone git@github.com:ragibasif/rule110.git
+```
+
+Make sure you have `gcc` and `make` installed.
+
+### Build the project
+
+```bash
+make
+```
+
+### Clean build files
+
+```bash
+make clean
+```
+
+### Run the executable
+
+```bash
+./rule110
+```
---
+++ rule110.c
@@ -0,0 +1,135 @@
+// rule110.h
+// https://en.wikipedia.org/wiki/Rule_110
+//
+// Rules:
+// Current pattern:             | 111 | 110 | 101 | 100 | 011 | 010 | 001 | 000
+// New state for center cell:   |  0  |  1  |  1  |  0  |  1  |  1  |  1  |  0
+// 7 -> 0
+// 6 -> 1
+// 5 -> 1
+// 4 -> 0
+// 3 -> 1
+// 2 -> 1
+// 1 -> 1
+// 0 -> 0
+
+#include "rule110.h"
+#include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+enum Rule {
+    ZERO = false,
+    ONE = true,
+    TWO = true,
+    THREE = true,
+    FOUR = false,
+    FIVE = true,
+    SIX = true,
+    SEVEN = false
+};
+
+#define MAX_BUFFER_SIZE 32
+
+int buffer[MAX_BUFFER_SIZE] = {0};
+
+bool check_last_three_bits(unsigned int n) {
+    unsigned int mask = (1 << 3) - 1;
+    unsigned int last_3_bits = n & mask;
+    bool result = true;
+    switch (last_3_bits) {
+    case 0:
+        result = ZERO;
+        break;
+    case 1:
+        result = ONE;
+        break;
+    case 2:
+        result = TWO;
+        break;
+    case 3:
+        result = THREE;
+        break;
+    case 4:
+        result = FOUR;
+        break;
+    case 5:
+        result = FIVE;
+        break;
+    case 6:
+        result = SIX;
+        break;
+    case 7:
+        result = SEVEN;
+        break;
+    default:
+        break;
+    }
+    return result;
+}
+unsigned int reverse_bits(unsigned int n) {
+    unsigned int result = n;
+    int size = sizeof(n) * CHAR_BIT - 1; // extra shift needed at the end
+    for (n >>= 1; n; n >>= 1) {
+        result <<= 1;
+        result |= n & 1;
+        size--;
+    }
+    result <<= size; // shift when n's highest bits are zero
+    return result;
+}
+
+unsigned int get_next(unsigned int n) {
+    unsigned int result = 0;
+    while (n) {
+        if (check_last_three_bits(n)) {
+            result <<= 1;
+            result |= 1;
+        } else {
+            result <<= 1;
+            result |= 0;
+        }
+        n >>= 1;
+    }
+    result = reverse_bits(result);
+
+    return result;
+}
+
+void set_buffer(unsigned int n) {
+    memset(buffer, 0, MAX_BUFFER_SIZE);
+    size_t i;
+    i = 0;
+    while (n && i < MAX_BUFFER_SIZE) {
+        buffer[i++] = n & 1;
+        n >>= 1;
+    }
+}
+
+void display_buffer(void) {
+    size_t i;
+    for (i = 0; i < MAX_BUFFER_SIZE; i++) {
+        if (buffer[i]) {
+            putchar('X');
+        } else {
+            putchar(' ');
+        }
+    }
+    putchar('\n');
+}
+
+int main(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+    int repeat = 32;
+    unsigned int n = 0x80000000;
+    set_buffer(n);
+    while (repeat--) {
+        display_buffer();
+        n = get_next(n);
+        set_buffer(n);
+    }
+    return 0;
+}
